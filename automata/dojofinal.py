286666be4c99739792518c14baff547ed195334e
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-10-16 11:07:12 +0100

    make it possible to pass arguments to play around with different rules easily

--- automata/dojofinal.py
+++ automata/dojofinal.py
@@ -1,3 +1,8 @@
+__metaclass__ = type
+
+import argparse
+import sys
+
 DEFAULT_RULE = 24
 DEFAULT_LEN = 3
 
@@ -25,7 +30,7 @@ def indexes_centered(center, num, tot_len):
             yield i % tot_len
 
 
-class Line(object):
+class Line:
     def __init__(self, rule=DEFAULT_RULE, rule_len=DEFAULT_LEN):
         self.rule_bits = rule_len
         self.rule_len = 2 ** rule_len
@@ -41,6 +46,28 @@ class Line(object):
 
     def next(self, current):
         return [self._next(current, idx) for idx in range(len(current))]
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='play around with cellular automata')
+
+    parser.add_argument('-r', '--rule',
+        type=int,
+        default=DEFAULT_RULE)
+
+    parser.add_argument('start',
+        type=int,
+        help='starting point')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    ns = parse_arguments()
+    curr = int_to_rule(ns.start, 2 ** DEFAULT_LEN)
+    while True:
+        print(','.join(map(str, curr)))
+        curr = Line(rule=ns.rule).next(curr)
 
 
 # Local Variables:
